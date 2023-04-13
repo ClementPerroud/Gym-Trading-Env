7e83fcbae4bcf470fa751dd0013f878cb44cb2ab
ClementPerroud <59481444+ClementPerroud@users.noreply.github.com>
2023-04-13 15:15:38 +0200

    Disable env checker

--- src/gym_trading_env/__init__.py
+++ src/gym_trading_env/__init__.py
@@ -2,10 +2,12 @@ from gymnasium.envs.registration import register
 
 register(
     id='TradingEnv',
-    entry_point='gym_trading_env.environments:TradingEnv'
+    entry_point='gym_trading_env.environments:TradingEnv',
+    disable_env_checker = True
 )
 register(
     id='MultiDatasetTradingEnv',
-    entry_point='gym_trading_env.environments:MultiDatasetTradingEnv'
+    entry_point='gym_trading_env.environments:MultiDatasetTradingEnv',
+    disable_env_checker = True
 )
  
