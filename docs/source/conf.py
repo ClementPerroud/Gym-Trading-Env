c3ab6c4daffe9669a6c323fb932b68e8f8d0cae7
Clément Perroud <59481444+ClementPerroud@users.noreply.github.com>
2023-04-23 23:01:39 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,3 +35,16 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+html_theme_options = {
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
