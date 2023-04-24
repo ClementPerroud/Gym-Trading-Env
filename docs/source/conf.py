fc4b5c218a5959e5cdee26963be94951a9f44821
Clément Perroud <59481444+ClementPerroud@users.noreply.github.com>
2023-04-24 16:55:58 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..src/.'))
+sys.path.insert(0, os.path.abspath('../..src'))
 # -- Project information
 
 project = 'Gym Trading Env'
