bf96bbfa4753eebf9d2616d8bac8d1d924e5b5f7
Clément Perroud <59481444+ClementPerroud@users.noreply.github.com>
2023-04-25 01:31:10 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
-html_logo = "image/logo_dark-bg.png"
+html_logo = "images/logo_dark-bg.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
