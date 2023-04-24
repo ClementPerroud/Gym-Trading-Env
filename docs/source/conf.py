3edfe68aa3ef07cf5e43af38fcac6e959260fa0b
Clément Perroud <59481444+ClementPerroud@users.noreply.github.com>
2023-04-24 15:13:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,19 +13,11 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx_copybutton',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
@@ -33,11 +25,6 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
-html_theme_options = {
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': -1,
-}
